e83efa2ee58a9c289b64c8318d13ea65d25551a1
Duan Imperatori <duan.imperatori@projjetta.com>
2022-12-17 20:38:52 -0300

    commit
    Signed-off-by: Duan Imperatori <duan.imperatori@projjetta.com>

--- script/lib.py
+++ script/lib.py
@@ -1,5 +1,12 @@
 
+from pyspark import SparkConf, SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import StructType, StructField, LongType, StringType, DoubleType, DecimalType, TimestampType, DataType, IntegerType
+from pyspark.sql.functions import col, lit, trim, substring, concat, udf, upper, initcap
+from datetime import datetime
+import os
 
+LAKE_HOME = os.getenv("LAKE_HOME", "/spark/home")
 
 def create_sk(spark, df, key_column_name, table_name):
     sk = {}
@@ -20,3 +27,9 @@ def create_sk(spark, df, key_column_name, table_name):
 
 def locate_sk(mapping: dict):
     return udf(lambda x: mapping.get(x), IntegerType())
+
+
+def map_sk(spark, table_name):
+    sk_table = spark.read.csv('{}/dataset/e-commerce/02_surrogate_key/sk_{}.csv'.format(LAKE_HOME, table_name), header=True)
+    dict_sk = sk_table.rdd.map(lambda x: (x[0], int(x[1]))).collectAsMap()
+    return dict_sk
